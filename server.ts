ddbc019ce3af209b9dbeb4d0d469154e04a6b8f7
aawil <aawilliam15@gmail.com>
2024-10-09 02:07:55 -0500

    fix

--- server.ts
+++ server.ts
@@ -20,9 +20,17 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('**', express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: 'index.html',
+  
+  
+  //  server.get('**', express.static(browserDistFolder, {
+  //    maxAge: '1y',
+  //    index: 'index.html',
+  //  }));
+
+    // Primero, servir archivos estáticos
+  server.use(express.static(browserDistFolder, {
+      maxAge: '1y',
+      index: 'index.html',
   }));
 
   // All regular routes use the Angular engine
